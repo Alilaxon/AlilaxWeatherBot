5dec84099fa42787f39c911a506a0e61a18cec0c
Alilaxon <firkraagin@gmail.com>
2021-12-24 18:59:45 +0200

    second commint

--- src/test/java/Bot.java
+++ src/test/java/Bot.java
@@ -1,12 +1,9 @@
 import Weather.Weather;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
-import org.telegram.telegrambots.meta.api.methods.BotApiMethod;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
-import org.telegram.telegrambots.meta.api.methods.stickers.CreateNewStickerSet;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
-import java.io.Serializable;
 
 class Bot extends TelegramLongPollingBot {
 
